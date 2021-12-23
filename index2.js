41318e846a9a513b4ddf7493ca550491e7fee129
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-12-23 19:54:36 +0100

    Created different routes

--- index2.js
+++ index2.js
@@ -1,23 +1,46 @@
 // npm install + require
 const express = require('express');
 const bodyParser = require('body-parser')
-const { MongoClient } = require('mongodb');
+//const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require("cors");
 
 // Create MongoClient to acces database cluster
-const client = new MongoClient(process.env.uri);
+//const client = new MongoClient(process.env.URI);
 
 // Port variable for Heroku
-const port = process.env.port;
+const port = process.env.PORT;
 
 // Create variables
 const app = express();
-const db = client.db(process.env.db);
-const collection = db.collection("user_data")
+//const db = client.db(process.env.DB);
+//const collection = db.collection(process.env.COL)
 
 // executing code using middlewares
 app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.redirect("/info.html");
+});
+
+app.get('/api/user_data', (req, res) => {
+    
+});
+
+app.put('/api/user_data/:id', (req, res) => {
+    
+});
+
+app.post('/api/newUser', (req, res) => {
+    
+});
+
+app.delete('/api/user_data/:id', (req, res) => {
+    
+});
+
+app.listen(port, () => {
+    console.log(`My first REST API Example app listening at http://localhost:${port}`)
+  })
